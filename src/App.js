4e1b77b26f07a6e2cf94e3e08d87cb72327abdf2
noah <noahjoeris@gmail.com>
2023-04-21 21:19:35 +0200

    update appbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
-import logo from "./logo.svg";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Avatar, Badge, styled } from "@mui/material";
 import "./App.css";
 import "./NavBar";
 import NavBar from "./NavBar";
+
+const imgPath = "ich.jpg";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 1px ${theme.palette.background.paper}`,
+    width: "5rem",
+    height: "5rem",
+    borderRadius: "50%",
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          variant="dot"
+        >
+          <Avatar
+            className="App-logo"
+            alt="Noah Joeris"
+            src={imgPath}
+            sx={{ width: "20rem", height: "20rem" }}
+          />
+        </StyledBadge>
+
         <p>Website in Progress</p>
       </header>
     </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,14 @@
-import { AppBar, Box, MenuItem, Toolbar, Typography } from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LanguageIcon from "@mui/icons-material/Language";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const NavBar = () => (
   <Box flexGrow={1}>
@@ -6,16 +16,39 @@ const NavBar = () => (
       <Toolbar>
         <Typography
           align="left"
+          fontFamily="Babylonica"
           component="div"
           flexGrow={1}
-          variant="h6"
+          variant="h4"
           color="inherit"
         >
           Noah Joeris
         </Typography>
         <MenuItem color="inherit">
-          <Typography>About Me</Typography>
+          <Typography variant="button">Portfolio</Typography>
         </MenuItem>
+        <MenuItem color="inherit">
+          <Typography variant="button">About Me</Typography>
+        </MenuItem>
+        <MenuItem color="inherit">
+          <Typography variant="button">Contact</Typography>
+        </MenuItem>
+        <IconButton>
+          <DarkModeIcon color="inherit" />
+        </IconButton>
+        <IconButton>
+          <Badge
+            badgeContent={
+              <img
+                src="flag_eng.svg"
+                alt=""
+                style={{ width: "150%", height: "150%" }}
+              />
+            }
+          >
+            <LanguageIcon />
+          </Badge>
+        </IconButton>
       </Toolbar>
     </AppBar>
   </Box>
