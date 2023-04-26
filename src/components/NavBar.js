33f695b750c19704fb7e47de169a6f0868d6be53
noah <noahjoeris@gmail.com>
2023-04-26 20:11:54 +0200

    refactor folder structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LanguageIcon from "@mui/icons-material/Language";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+const NavBar = ({ darkModeEnabled, setDarkModeEnabled }) => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Box flexGrow={1}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            align="left"
+            fontFamily="Babylonica"
+            component="div"
+            flexGrow={1}
+            variant="h4"
+            color="inherit"
+          >
+            Noah Joeris
+          </Typography>
+          <MenuItem color="inherit">
+            <Typography variant="button">{t("portfolioNav")}</Typography>
+          </MenuItem>
+          <MenuItem color="inherit">
+            <Typography variant="button">{t("aboutMeNav")}</Typography>
+          </MenuItem>
+          <MenuItem color="inherit">
+            <Typography variant="button">{t("contactNav")}</Typography>
+          </MenuItem>
+          <IconButton onClick={() => setDarkModeEnabled(!darkModeEnabled)}>
+            <DarkModeIcon color="inherit" />
+          </IconButton>
+          <IconButton
+            onClick={() => {
+              i18n.changeLanguage(i18n.language === "en" ? "de" : "en");
+            }}
+          >
+            <Badge
+              badgeContent={
+                <img
+                  src={i18n.language === "en" ? "flag_eng.svg" : "flag_ger.svg"}
+                  alt=""
+                  style={{ width: "150%", height: "150%" }}
+                />
+              }
+            >
+              <LanguageIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default NavBar;
