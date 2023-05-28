3def9fda6615dbafbbfe20c7398dab67d8829f4d
noah <noahjoeris@gmail.com>
2023-05-28 16:49:28 +0200

    fix recaptcha issues

--- src/components/MessageStepper.js
+++ src/components/MessageStepper.js
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { TextField, useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Step from "@mui/material/Step";
@@ -7,7 +7,7 @@ import Stepper from "@mui/material/Stepper";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
-import { CAPTCHA_SITE_KEY, sendContactMessage } from "../services/email";
+import { CAPTCHA_SITE_KEY } from "../services/email";
 
 const steps = ["Message", "Name", "Email"];
 
@@ -17,11 +17,14 @@ const validateInputs = (message, name, email) => {
 };
 
 export default function MessageStepper() {
+  const theme = useTheme();
   const [activeStep, setActiveStep] = useState(0);
   const [recaptchaOpened, setRecaptchaOpened] = useState(false);
-  const [message, setMessage] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("testmessage");
+  const [name, setName] = useState("test");
+  const [email, setEmail] = useState("w@gmail.com");
+
+  let recaptchaRef = null;
 
   return (
     <Box>
@@ -41,102 +44,108 @@ export default function MessageStepper() {
           );
         })}
       </Stepper>
-      {activeStep === steps.length ? (
-        <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            Message was sent successfully!
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button onClick={() => setActiveStep(0)} color="inherit">
-              Send another message
-            </Button>
-          </Box>
-        </>
-      ) : (
-        <>
-          <Box>
-            {activeStep === 0 && (
-              <Box>
-                <TextField
-                  multiline
-                  fullWidth
-                  label="Message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  minRows={6}
-                  maxRows={8}
-                />
-              </Box>
-            )}
-            {activeStep === 1 && (
+      {!recaptchaOpened && (
+        <Box>
+          {activeStep === 0 && (
+            <Box>
               <TextField
-                label="Your Name"
+                multiline
                 fullWidth
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                label="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                minRows={6}
+                maxRows={8}
               />
-            )}
-            {activeStep === 2 && (
-              <TextField
-                label="Your Email"
-                fullWidth
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            )}
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              pt: 2,
-            }}
-          >
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={() => {
-                setActiveStep((prevActiveStep) => prevActiveStep - 1);
-                setRecaptchaOpened(false);
-              }}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button
-              onClick={() => {
-                if (
-                  activeStep === steps.length - 1 &&
-                  validateInputs(message, name, email)
-                ) {
-                  setRecaptchaOpened(true);
-                } else {
-                  setActiveStep((prevActiveStep) => prevActiveStep + 1);
-                }
-              }}
-              color="inherit"
-              disabled={
-                activeStep === steps.length - 1 &&
-                !validateInputs(message, name, email)
-              }
-            >
-              {activeStep === steps.length - 1 ? "Send 🚀" : "Next"}
-            </Button>
-          </Box>
-          {recaptchaOpened && (
-            <ReCAPTCHA
-              sitekey={CAPTCHA_SITE_KEY}
-              onChange={(captchaValue) => {
-                sendContactMessage(name, email, message, captchaValue);
-                setRecaptchaOpened(false);
-                setActiveStep((prevActiveStep) => prevActiveStep + 1);
-              }}
+            </Box>
+          )}
+          {activeStep === 1 && (
+            <TextField
+              label="Your Name"
+              fullWidth
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           )}
-        </>
+          {activeStep === 2 && !recaptchaOpened && (
+            <TextField
+              label="Your Email"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          )}
+          {activeStep === steps.length && (
+            <>
+              <Typography sx={{ mt: 2, mb: 1 }}>
+                Message was sent successfully!
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+                <Box sx={{ flex: "1 1 auto" }} />
+                <Button onClick={() => setActiveStep(0)} color="inherit">
+                  Send another message
+                </Button>
+              </Box>
+            </>
+          )}
+        </Box>
       )}
+      {!recaptchaOpened && activeStep < steps.length && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            pt: 2,
+          }}
+        >
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={() => {
+              setActiveStep((prevActiveStep) => prevActiveStep - 1);
+            }}
+            sx={{ mr: 1 }}
+          >
+            Back
+          </Button>
+          <Box sx={{ flex: "1 1 auto" }} />
+          <Button
+            onClick={() => {
+              if (
+                activeStep === steps.length - 1 &&
+                validateInputs(message, name, email)
+              ) {
+                setRecaptchaOpened(true);
+              } else {
+                setActiveStep((prevActiveStep) => prevActiveStep + 1);
+              }
+            }}
+            color="inherit"
+            disabled={
+              activeStep === steps.length - 1 &&
+              !validateInputs(message, name, email)
+            }
+          >
+            {activeStep === steps.length - 1 ? "Send 🚀" : "Next"}
+          </Button>
+        </Box>
+      )}
+
+      <ReCAPTCHA
+        ref={(r) => (recaptchaRef = r)}
+        theme={theme.palette.mode === "light" ? "light" : "dark"}
+        sitekey={CAPTCHA_SITE_KEY}
+        hidden={!recaptchaOpened}
+        onChange={(captchaValue) => {
+          if (captchaValue) {
+            //sendContactMessage(name, email, message, captchaValue);
+            console.log("Message sent");
+            setActiveStep((prevActiveStep) => prevActiveStep + 1);
+            setRecaptchaOpened(false);
+            recaptchaRef?.reset();
+          }
+        }}
+      />
     </Box>
   );
 }
