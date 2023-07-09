551d7bd5019d367a68b23350501914f0abd7348f
Noah Joeris <noahjoeris@gmail.com>
2023-07-09 13:24:14 +0200

    migrate some files to .tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
-import { Box, Typography, useTheme } from "@mui/material";
 import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
 import ContactsSideBar from "./ContactsSideBar";
+
+
+const lightBackground : string = "linear-gradient(to top right,  #f0f0f0 80%, #000bae 120%)";
+const darkBackground : string = "linear-gradient(to top right,  #070707 80%, #000bae 120%)";
 
 const Footer: React.FC = () => {
   const theme = useTheme();
-  const gradientBackground =
-    theme.palette.mode === "light"
-      ? "linear-gradient(to top right,  #f0f0f0 80%, #000bae 120%)"
-      : "linear-gradient(to top right,  #070707 80%, #000bae 120%)";
 
   return (
     <Box
@@ -16,7 +16,9 @@ const Footer: React.FC = () => {
       height="10%"
       width="100%"
       justifyContent="space-evenly"
-      sx={{ background: gradientBackground }}
+      sx={{ background: theme.palette.mode === "light"
+      ? lightBackground
+      : darkBackground }}
     >
       <Box
         display="flex"
@@ -24,8 +26,10 @@ const Footer: React.FC = () => {
         alignItems="center"
         paddingLeft={"0.5rem"}
       >
-        <Typography>© 2023 Noah Joeris. All rights reserved.</Typography>
-        <ContactsSideBar alignment="row" />
+        <Typography>
+          © {new Date().getFullYear()} Noah Joeris. All rights reserved.
+        </Typography>
+        <ContactsSideBar />
       </Box>
       <Typography sx={{ backgroundColor: "darkgray" }}>
         Made with ❤️ in React.js
